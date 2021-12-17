1d17c99ddca648dcdd3fd44e1066d477b9b3351f
shanmathimanickam <49234896+shanmathimanickam@users.noreply.github.com>
2021-12-17 13:59:18 +0530

    Update data_sources.tf

--- data_sources.tf
+++ data_sources.tf
@@ -9,4 +9,4 @@ data "azurerm_subnet" "mcds_subnet" {
   name                 = var.subnet
   virtual_network_name = data.azurerm_virtual_network.mcds_net.name
   resource_group_name  = data.azurerm_resource_group.mcds_test.name
-}
+
